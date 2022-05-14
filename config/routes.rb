8fa697408da69f193dcb2a2e4340d3494cfbf144
EC2 Default User <ec2-user@ip-172-31-41-162.ec2.internal>
2022-05-14 11:00:42 +0000

    [Add]課題手順21~26行目までの体裁

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :books, only: [:index, :show, :edit]
   resources :users, only: [:index, :show, :edit]
   root to: "homes#top"
   get '/homes/about' => 'homes#about', as: 'about'
   #『/users/~』のroutesとpathの
-  devise_for :users
 end
