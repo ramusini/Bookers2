e990bedeeb0b3e628ebc27667998ec3d832d1462
EC2 Default User <ec2-user@ip-172-31-41-162.ec2.internal>
2022-05-15 11:23:59 +0000

    [Add]課題手順27~32行目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,36 @@
 class BooksController < ApplicationController
   def index
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params) #下でbook_paramsのストロングパラメータメソッドを作成しないと機能しない。
+    @book.user_id = current_user.id #ここが不在の場合、bookモデルのuser_idが不在になり、saveできない。
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
+    #@book = Book.newを指定したい。でないとこの画面でsubmitがpatchになる。
   end
 
   def edit
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books, only: [:index, :show, :edit]
-  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:index, :show, :edit, :create, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
   root to: "homes#top"
   get '/homes/about' => 'homes#about', as: 'about'
   #『/users/~』のroutesとpathの
